500a4bbe075a8ce09134fdb53b23f572e4ecd120
Mohamed <elsebaey95@outlook.com>
2025-01-23 20:03:51 +0200

    ..

--- components/sign-in-up/SignIn.tsx
+++ components/sign-in-up/SignIn.tsx
@@ -8,7 +8,6 @@ import { useActionState, useEffect, useState } from "react";
 import { signInFormAction } from "@/actions/signInAction";
 
 import SubmitButton from "@/ui/SubmitButton";
-import InputFelid from "@/ui/InputFelid";
 
 import { signIn } from "next-auth/react";
 
@@ -218,3 +217,45 @@ function SignIn() {
 }
 
 export default SignIn;
+
+
+const InputFelid = ({
+  label,
+  name,
+  type,
+  placeholder,
+  onChange,
+  required,
+  errorStyle,
+}: {
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
+  onChange?: ()=>void;
+  required?: boolean;
+  errorStyle?: boolean;
+}) => {
+  const style = errorStyle
+    ? "border-red-500 focus:border-red-500 ring-red-500/50"
+    : "border-gray-300 focus:border-background2/40 ring-background2/20";
+  return (
+    <div>
+      <label
+        htmlFor={name}
+        className="mb-1 inline-block font-medium text-background"
+      >
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        required={required}
+        type={type}
+        className={`relative mb-2 h-12 w-full flex-grow appearance-none rounded border bg-white px-4 shadow-sm transition duration-200  focus:outline-none focus:ring ${style}`}
+      />
+    </div>
+  );
+};
